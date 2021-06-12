b4f6ba0092719613358f952c650282674a006632
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-06-12 15:11:12 -0500

    axios call

--- utils/GoogleBooksAPI.js
+++ utils/GoogleBooksAPI.js
@@ -1,8 +1,15 @@
 //import axios
+import axios from 'axois'
 //create const with api path
+const api = axios.create({
+   BASEURL: "https://www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=",
+})
 //create const with api key
+const key = 'API_KEY'
 
 function searchBooks(params) {
     //build a url with string interpolation where q=+param and use axios to call it
+   return axios.get( api + `${key}`)
 }
+
 export default searchBooks;
