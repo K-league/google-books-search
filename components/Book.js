a4cd9f4cf60ae6ec6a3e144e42fb5a4c16d3d6f0
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-07-01 13:00:22 -0500

    Book react component, fresh README

--- components/Book.js
+++ components/Book.js
@@ -1,1 +1,32 @@
-// create component with a single book display
+import React, { Component } from "react";
+import API from "../utils/GoogleBooksAPI";
+
+class Book extends Component {
+
+    state = {
+        savedBooks: [],
+    }
+
+    componentDidMount() {
+        API.savedBooks()
+            .then(savedBooks => this.setState({ savedBooks: savedBooks }))
+            .catch(err => console.error(err));
+    }
+
+    handleSave = book => {
+
+        if (this.state.savedBooks.map(book => book._id).includes(book._id)) {
+
+        }
+
+    }
+
+    render () {
+        return (
+            <div>Book Component</div>
+        )
+    }
+
+
+}
+export default Book;
